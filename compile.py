012282db3207bb1a7da4dfe998583ca51fbc86fc
Alessandro Pelliciari <avastreg@webranking>
2012-07-12 18:17:05 +0200

    fix al compile

--- compile.py
+++ compile.py
@@ -8,18 +8,6 @@
 #!/usr/bin/env python
 
 import os
-import subprocess
-
-def saveVersion (filename):
-    f = open(filename,  'w')
-    f.write( str(retrieveVersion() ) )
-    f.close()
-
-def retrieveVersion ():
-    command = r"git --git-dir=C:\Users\alessandro\Desktop\vhost_generator\.git rev-parse HEAD"
-    return subprocess.check_output(command.split(), shell=True).rstrip('\r\n')
-
-
 
 ## 'python Makespec.py -w -o C:\Home\Python\check-301s\ -n "'+ projectname + '" --icon="C:\Home\Python\check-301s\salta-ai-risultati.ico" C:\Home\Python\check-301s\__init__.py'
 
@@ -27,16 +15,10 @@ pyinstallerfolder = 'C:/Home/Python/pyinstaller-1.5-rc1'
 projectfolder = os.getcwd() + '\\'
 executablesfolder = projectfolder + 'executables\\'
 
-print projectfolder
-version = str(retrieveVersion())
 projectname = 'vhost-conf-generator' + "-" + retrieveVersion()
 
-makespec = 'python Makespec.py -F -X -o ' + executablesfolder + ' -n "'+ projectname + '" --icon="' + projectfolder + 'salta-ai-risultati.ico" ' + projectfolder + 'setup.py'
-print makespec
-buildspec = 'python Build.py ' + executablesfolder +  projectname + '.spec'
+buildspec = 'python Build.py ' + projectfolder + 'spec.py'
 
 if __name__ == "__main__":
     os.chdir(pyinstallerfolder)
-    os.system(makespec)
     os.system(buildspec)
-    saveVersion(projectfolder + 'version')
