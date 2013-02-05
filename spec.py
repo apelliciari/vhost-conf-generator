5ef233ef501036538e3601e1a748db0bb865a3a1
Alessandro Pelliciari <isac.newton@gmail.com>
2013-02-05 12:22:47 +0100

    fix creazione exe

--- spec.py
+++ spec.py
@@ -1,16 +1,22 @@
 # -*- mode: python -*-
-f = open("C:\\Users\\alessandro\\Desktop\\vhost_generator\\version")
+#from pack import projectfolder, version, pyinstallerfolder
+
+projectfolder = 'C:/Home/vhost_generator/'
+pyinstallerfolder = 'C:/Home/Python/pyinstaller-1.5-rc1'
+
+
+f = open(projectfolder + 'version')
 version = f.read()
 f.close()
 
-a = Analysis([os.path.join(HOMEPATH,'support\\_mountzlib.py'), os.path.join(HOMEPATH,'support\\useUnicode.py'), 'C:\\Users\\alessandro\\Desktop\\vhost_generator\\main.py'],
-             pathex=['C:\\Home\\Python\\pyinstaller-1.5-rc1'])
+a = Analysis([os.path.join(HOMEPATH,'support\\_mountzlib.py'), os.path.join(HOMEPATH,'support\\useUnicode.py'), projectfolder + 'main.py'],
+             pathex=[pyinstallerfolder])
 
-a.datas += [("templates/user.tpl",'C:\\Users\\alessandro\\Desktop\\vhost_generator\\templates\\user.tpl',"DATA")]
-a.datas += [("templates/logrotate.tpl",'C:\\Users\\alessandro\\Desktop\\vhost_generator\\templates\\logrotate.tpl',"DATA")]
-a.datas += [("templates/samba.tpl",'C:\\Users\\alessandro\\Desktop\\vhost_generator\\templates\\samba.tpl',"DATA")]
-a.datas += [("templates/cmd.tpl",'C:\\Users\\alessandro\\Desktop\\vhost_generator\\templates\\cmd.tpl',"DATA")]
-a.datas += [("templates/vhost.tpl",'C:\\Users\\alessandro\\Desktop\\vhost_generator\\templates\\vhost.tpl',"DATA")]
+a.datas += [("templates/user.tpl", projectfolder + 'templates\\user.tpl',"DATA")]
+a.datas += [("templates/logrotate.tpl", projectfolder + 'templates\\logrotate.tpl',"DATA")]
+a.datas += [("templates/samba.tpl", projectfolder + 'templates\\samba.tpl',"DATA")]
+a.datas += [("templates/cmd.tpl", projectfolder + 'templates\\cmd.tpl',"DATA")]
+a.datas += [("templates/vhost.tpl", projectfolder + 'templates\\vhost.tpl',"DATA")]
 
 pyz = PYZ(a.pure)
 exe = EXE( pyz,
@@ -22,5 +28,6 @@ exe = EXE( pyz,
           debug=False,
           strip=False,
           upx=True,
-          console=True , icon='C:\\Users\\alessandro\\Desktop\\vhost_generator\\salta-ai-risultati.ico')
+          console=True , icon=projectfolder + 'salta-ai-risultati.ico')
+
 
